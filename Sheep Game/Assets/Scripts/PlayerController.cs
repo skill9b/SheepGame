0a94816668f631194706d3b9ed78671ee24f2013
Vivian Xu <viv.xu@windowslive.com>
2019-10-09 19:49:16 +1300

    PlayerController

--- Sheep Game/Assets/Scripts/PlayerController.cs
+++ Sheep Game/Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerContoller : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
