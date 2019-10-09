c391b53e428469171dbe8de3c66648310df22e44
Vivian Xu <viv.xu@windowslive.com>
2019-10-09 20:56:08 +1300

    Changes
    SheepController added
    SpawningController added
    Deleted Callum object
    Deleted yayeet object

---
+++ Sheep Game/Assets/Scripts/SheepController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepController : MonoBehaviour
+{
+    public float speed;
+    public int distanceFromEnemy;
+    public Transform target;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        distanceFromEnemy = 1;
+        speed = 1.0f;
+    }
+
+    void Update()
+    {
+
+        if (Vector2.Distance(transform.position, target.position) > distanceFromEnemy)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+}
---
+++ Sheep Game/Assets/Scripts/SpawningController.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawningController : MonoBehaviour
+{
+
+    // ******** VARIABLES ******** //
+    public enum SpawnState
+    {
+        SPAWNING,
+        WAITING,
+        COUNTING
+    }
+    private SpawnState state = SpawnState.COUNTING;
+
+    [System.Serializable]
+    public class Wave
+    {
+        public string name;
+        public Transform sheepEnemy;
+        public int count;
+        public float rate;
+    }
+
+    public Wave[] waves;
+    private int waveCount = 0;   // default is 0
+
+    public Transform[] spawnPoints;
+
+    public float timeBetweenWaves = 5.0f;   // 5 seconds
+    private float waveCountdown;
+
+    private float searchCountdown = 1.0f;
+
+
+    // ******** FUNCTIONS ******** //
+    void Start()
+    {
+        if (spawnPoints.Length == 0)
+        {
+            Debug.LogError("No spawn points referenced.");
+        }
+        waveCountdown = timeBetweenWaves;
+    }
+
+    void Update()
+    {
+
+        // Check if player has killed all the enemies
+        if (state == SpawnState.WAITING)
+        {
+            // Check if any enemies are still alive
+            if (!isEnemyAlive())
+            {
+                // Begin new round
+                startNextWave();
+                return;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        if (waveCountdown <= 0)
+        {
+            // Spawn wave condition
+            if (state != SpawnState.SPAWNING)
+            {
+                StartCoroutine(SpawnWave(waves[waveCount]));
+            }
+        }
+        else
+        {
+            waveCountdown -= Time.deltaTime;    // countsdown second by second
+        }
+    }
+
+    IEnumerator SpawnWave(Wave _wave)
+    {
+        Debug.Log("Spawning Wave: " + _wave.name);
+        state = SpawnState.SPAWNING;
+
+        // Spawn logic
+        for (int i = 0; i < _wave.count; i++)
+        {
+
+            //Transform enemyToSpawn;
+            //float rate = Random.Range(0.0f, 1.0f);
+
+            //if (rate < 0.6)
+            //{
+            //    enemyToSpawn = _wave.firstEnemy;
+            //}
+            //else if (rate < 0.8)
+            //{
+            //    enemyToSpawn = _wave.secondEnemy;
+
+            //}
+            //else
+            //{
+            //    enemyToSpawn = _wave.thirdEnemy;
+            //}
+
+            SpawnEnemy(_wave.sheepEnemy);
+            yield return new WaitForSeconds(1.0f / _wave.rate); // wait before spawning next enemy
+        }
+
+        state = SpawnState.WAITING;
+
+        yield break;
+    }
+
+    void SpawnEnemy(Transform _enemy)
+    {
+        if (spawnPoints.Length == 0)
+        {
+            Debug.LogError("No spawn points referenced.");
+        }
+
+        Transform _sp = spawnPoints[Random.Range(0, spawnPoints.Length)];
+        Instantiate(_enemy, _sp.position, _sp.rotation);
+        Debug.Log("Spawning enemy: " + _enemy.name);
+    }
+
+
+    void startNextWave()
+    {
+        Debug.Log("Wave completed.");
+
+        state = SpawnState.COUNTING;
+        waveCountdown = timeBetweenWaves;
+
+        if (waveCount + 1 > waves.Length - 1)
+        {
+            waveCount = 0;
+            Debug.Log("All waves complete!");
+        }
+        else
+        {
+
+            waveCount++;
+        }
+    }
+
+    bool isEnemyAlive()
+    {
+        searchCountdown -= Time.deltaTime;
+        if (searchCountdown <= 0.0f)
+        {
+            searchCountdown = 1.0f;
+            if (GameObject.FindGameObjectsWithTag("Enemy").Length == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
