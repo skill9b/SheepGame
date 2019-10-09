b86e3c929d81eef18d1866728fdbd6d27e6c39d0
Jay <55824029+skill9b@users.noreply.github.com>
2019-10-09 22:23:22 +1300

    Added Player Movement

--- Sheep Game/Assets/Scripts/PlayerController.cs
+++ Sheep Game/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed;
+    private Rigidbody2D rb2d;
+
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);
     }
+
 }
