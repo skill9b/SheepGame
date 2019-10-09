d44d34d0ce8008a04c0da61ac462991078b86d29
Jay <55824029+skill9b@users.noreply.github.com>
2019-10-09 22:45:30 +1300

    Better Movement

--- Sheep Game/Assets/Scripts/PlayerController.cs
+++ Sheep Game/Assets/Scripts/PlayerController.cs
@@ -17,7 +17,7 @@ public class PlayerController : MonoBehaviour
         float moveHorizontal = Input.GetAxis ("Horizontal");
         float moveVertical = Input.GetAxis ("Vertical");
 
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        Vector2 movement = new Vector2(0, moveVertical);
         rb2d.AddForce(movement * speed);
     }
 
