01d970688f901a4c47b3ad0f5e6e1dc88409f5f7
Jay <55824029+skill9b@users.noreply.github.com>
2019-10-09 23:36:04 +1300

    Changed speed to 4

--- Sheep Game/Assets/Scripts/PlayerController.cs
+++ Sheep Game/Assets/Scripts/PlayerController.cs
@@ -4,43 +4,56 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
-    private Rigidbody2D rb2d;
-    private bool moving;
-
-    void Start()
-    {
-        rb2d = GetComponent<Rigidbody2D>();
-    }
-
+       public float speed;
+       private Rigidbody2D rb2d;
+    // private bool moving;
+    //
+        void Start()
+        {
+            rb2d = GetComponent<Rigidbody2D>();
+        }
+        
+        
+    // void Update()
+    // {
+    //     
+    // }
+    //
+    // void FixedUpdate()
+    // {
+    //     float moveVertical = Input.GetAxis ("Vertical");
+    //
+    //     if (moveVertical != 0)
+    //     {
+    //         moving = true;
+    //     }
+    //     else
+    //     {
+    //         moving = false;
+    //     }
+    //
+    //     Vector2 movement = new Vector2(0, moveVertical);
+    //     if (moving)
+    //     {
+    //         rb2d.AddForce(movement * speed);
+    //     }
+    //     else
+    //     {
+    //         rb2d.velocity = Vector2.zero;
+    //     }
+    // }
 
     void Update()
     {
-        
-    }
+        // Getting user input 
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
 
-    void FixedUpdate()
-    {
-        float moveVertical = Input.GetAxis ("Vertical");
-
-        if (moveVertical != 0)
-        {
-            moving = true;
-        }
-        else
-        {
-            moving = false;
-        }
-
-        Vector2 movement = new Vector2(0, moveVertical);
-        if (moving)
-        {
-            rb2d.AddForce(movement * speed);
-        }
-        else
-        {
-            rb2d.velocity = Vector2.zero;
-        }
+        // Player Movement
+        Vector2 position = rb2d.position;
+        position.x += speed * horizontal * Time.deltaTime;
+        position.y += speed * vertical * Time.deltaTime;
+        rb2d.MovePosition(position);
     }
 
 }
