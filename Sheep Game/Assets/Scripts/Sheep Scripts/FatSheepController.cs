826aaa57e48bcf96b8880a1aad987b535da21172
Vivian Xu <viv.xu@windowslive.com>
2019-10-10 15:19:56 +1300

    Added new Sheep Prefabs & Scripts

---
+++ Sheep Game/Assets/Scripts/Sheep Scripts/FatSheepController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeSheepController : ParentSheepController
+{
+
+    public Transform tinySheepObject;
+
+    public override void Die()
+    {
+        // Instantiate(deathEffect, transform.position, Quaternion.identity);
+        // Play death sound
+        // GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>().AddWool(woolPoints);
+
+        Instantiate(tinySheepObject, transform.position, Quaternion.identity);
+
+        Destroy(gameObject);
+
+    }
+}
---
+++ Sheep Game/Assets/Scripts/Sheep Scripts/ParentSheepController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParentSheepController : MonoBehaviour
+{
+    public int health;
+    public float speed;
+    public int distanceFromEnemy;
+    public int woolPoints;
+    public Transform target;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        // Stop if certain distance from enemy
+        if (Vector2.Distance(transform.position, target.position) > distanceFromEnemy)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+
+    public virtual void Attack()
+    {
+
+    }
+
+    public virtual void Die()
+    {
+
+    }
+}
