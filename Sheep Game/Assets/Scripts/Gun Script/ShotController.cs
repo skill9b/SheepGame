8c978e51153821d40337927b850e3ce880be3481
BenMDS <unigeorge98@gmail.com>
2019-10-17 18:52:04 +1300

    Guns Working and SceneManager
    -Shotgun with a special script for the bullet
    -Arcgun
    
    -Scenemanager with 3 test scenes

---
+++ Sheep Game/Assets/Scripts/Gun Script/ShotController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotController : MonoBehaviour
+{
+    public GameObject Bullet;
+    public GameObject Gun;
+    public Camera camera;
+
+    [SerializeField] float Speed = 4;
+    [SerializeField] float Offset = 180;
+    [SerializeField] float FireRate = 1;
+    [SerializeField] int Mag = 5;
+    [SerializeField] float CooldowntimeFULL = 5f;
+    [SerializeField] int CooldowntimeNOTFULL = 2;
+    private int BulletCount = 0;
+
+    private Vector3 Target;
+    private float LastShot = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Target = camera.ScreenToWorldPoint(new Vector3(transform.position.x, (-Input.mousePosition.y) + Offset, transform.position.z));
+
+        Vector3 difference = Target - Gun.transform.position;
+        float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        Gun.transform.rotation = Quaternion.Euler(0, 0, rotationZ);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            float distance = difference.magnitude;
+            Vector2 direction = difference / distance;
+            direction.Normalize();
+
+            Fire(direction, rotationZ);
+        }
+    }
+
+    //WaitForSecond returns a IEnumerator type, which is why it's it's own function
+    IEnumerator Wait(float Seconds)
+    {
+        yield return new WaitForSeconds(Seconds);   //Scaled time (No Idea what that means)
+        BulletCount -= 1;   //The cooldown reduces bulletcount by 1
+    }
+
+    void Fire(Vector2 direction, float rotationZ)
+    {
+        if (Time.time > FireRate + LastShot)
+        {
+            if (BulletCount == Mag) //if the mag has been used up make player wait long
+            {
+                StartCoroutine(Wait(CooldowntimeFULL));
+            }
+
+            //Regular Shooting
+            if (BulletCount != Mag)
+            {
+                GameObject a = Instantiate(Bullet) as GameObject;
+                a.transform.position = Gun.transform.position;
+                a.transform.rotation = Quaternion.Euler(0, 180, -rotationZ);
+                a.GetComponent<Rigidbody2D>().velocity = direction * Speed;
+
+                BulletCount += 1;
+
+                if (Time.time > CooldowntimeNOTFULL + LastShot) //Reduce the "Heat" cool down by a bit everytime the player doesn't shoot"
+                {
+                    if (BulletCount != 0)
+                    {
+                        BulletCount -= 1;
+                    }
+
+                    {
+                        Debug.Log("hi uwu");    //Adds ambience to code :)
+                    }
+                }
+
+                LastShot = Time.time;
+                Debug.Log(BulletCount);
+            }
+        }
+    }
+}
---
+++ Sheep Game/Assets/Scripts/ShotgunBullet.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotgunBullet : MonoBehaviour
+{
+    public GameObject Sheep;
+    public SheepController SheepScript;
+
+    [SerializeField] float Seconds = 1;
+    [SerializeField] int Mag = 6;
+    
+    private int EnemyCount = 0;
+    private int Damage = 0;
+
+    IEnumerator Wait(float Seconds)
+    {
+        yield return new WaitForSecondsRealtime(Seconds);   //Scaled time (No Idea what that means)
+        Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        SheepScript = Sheep.GetComponent<SheepController>();
+
+    }
+
+    private void Update()
+    {
+        StartCoroutine(Wait(Seconds));
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            EnemyCount++;
+          
+            Damage = (Mag - EnemyCount) / EnemyCount;
+
+            if (EnemyCount > Mag)
+            {
+                SheepScript.health -= 1;
+            }
+
+            if (EnemyCount < Mag)
+            {
+                SheepScript.health -= Damage;
+            }
+
+            Destroy(gameObject);
+        }
+    }
+}
