4b297200cf53dedf3c2539f5a548e83eaae99dff
BenMDS <unigeorge98@gmail.com>
2019-10-17 14:06:32 +1300

    SceneController

---
+++ Sheep Game/Assets/Scenes/switchingscenes/SceneController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("a"))
+        {
+            SceneManager.LoadScene(0);
+            DontDestroyOnLoad(this.gameObject);
+        }
+
+        if (Input.GetKeyDown("s"))
+        {
+            SceneManager.LoadScene(1);
+            DontDestroyOnLoad(this.gameObject);
+        }
+
+        if (Input.GetKeyDown("d"))
+        {
+            SceneManager.LoadScene(2);
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
