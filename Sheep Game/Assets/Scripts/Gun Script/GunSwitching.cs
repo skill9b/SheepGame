2b6af187ef93ad9ad1c20244caeadac9969ba781
StarPatric <unigeorge98@gmail.com>
2019-10-23 18:20:31 +1200

    Gun Switching

---
+++ Sheep Game/Assets/Scripts/Gun Script/GunSwitching.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunSwitching : MonoBehaviour
+{
+    public GameObject ArcGun;
+    public GameObject LongGun;
+    public GameObject ShortGun;
+
+    private void Start()
+    {
+        ArcGun.SetActive(false);
+        ShortGun.SetActive(false);
+
+        LongGun.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            ShortGun.SetActive(false);
+            LongGun.SetActive(false);
+
+            ArcGun.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ShortGun.SetActive(false);
+            ArcGun.SetActive(false);
+
+            LongGun.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            ArcGun.SetActive(false);
+            LongGun.SetActive(false);
+
+            ShortGun.SetActive(true);
+        }
+    }
+}
