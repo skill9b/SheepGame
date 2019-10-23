5a11472fcd1bb17b9ede1640ceb5c641705a32be
StarPatric <unigeorge98@gmail.com>
2019-10-23 19:35:23 +1200

    Gun Switching UI

--- Sheep Game/Assets/Scripts/Gun Script/GunSwitching.cs
+++ Sheep Game/Assets/Scripts/Gun Script/GunSwitching.cs
@@ -1,19 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class GunSwitching : MonoBehaviour
 {
-    public GameObject ArcGun;
-    public GameObject LongGun;
-    public GameObject ShortGun;
+    public GameObject ArcGun; public Image Arc;
+    public GameObject LongGun; public Image Long;
+    public GameObject ShortGun; public Image Short;
+
 
     private void Start()
     {
-        ArcGun.SetActive(false);
-        ShortGun.SetActive(false);
+        ArcGun.SetActive(false); Color Temp = Arc.color; Temp.a = 0.3f; Arc.color = Temp;
+        ShortGun.SetActive(false); Temp = Short.color; Temp.a = 0.3f; Short.color = Temp;
 
-        LongGun.SetActive(true);
+        LongGun.SetActive(true); Temp = Long.color; Temp.a = 1.0f; Long.color = Temp;
     }
 
     // Update is called once per frame
@@ -21,26 +23,26 @@ public class GunSwitching : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            ShortGun.SetActive(false);
-            LongGun.SetActive(false);
+            ShortGun.SetActive(false); Color Temp = Short.color; Temp.a = 0.3f; Short.color = Temp;
+            LongGun.SetActive(false); Temp = Long.color; Temp.a = 0.3f; Long.color = Temp;
 
-            ArcGun.SetActive(true);
+            ArcGun.SetActive(true); Temp = Arc.color; Temp.a = 1.0f; Arc.color = Temp;
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            ShortGun.SetActive(false);
-            ArcGun.SetActive(false);
+            ShortGun.SetActive(false); Color Temp = Short.color; Temp.a = 0.3f; Short.color = Temp;
+            ArcGun.SetActive(false); Temp = Arc.color; Temp.a = 0.3f; Arc.color = Temp;
 
-            LongGun.SetActive(true);
+            LongGun.SetActive(true); Temp = Long.color; Temp.a = 1.0f; Long.color = Temp;
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-            ArcGun.SetActive(false);
-            LongGun.SetActive(false);
+            ArcGun.SetActive(false); Color Temp = Arc.color; Temp.a = 0.3f; Arc.color = Temp;
+            LongGun.SetActive(false); Temp = Long.color; Temp.a = 0.3f; Long.color = Temp;
 
-            ShortGun.SetActive(true);
+            ShortGun.SetActive(true); Temp = Short.color; Temp.a = 1.0f; Short.color = Temp;
         }
     }
 }
