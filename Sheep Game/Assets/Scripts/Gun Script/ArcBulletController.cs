22f83c0ea4f3f1e3a40192daca12d49ff62114b7
BenMDS <unigeorge98@gmail.com>
2019-10-29 11:47:14 +1300

    ArcBullet AOE is changable now

--- Sheep Game/Assets/Scripts/Gun Script/ArcBulletController.cs
+++ Sheep Game/Assets/Scripts/Gun Script/ArcBulletController.cs
@@ -7,6 +7,15 @@ public class ArcBulletController : MonoBehaviour
 
     public int damage;
 
+    public float ColX = 0.2f, ColY = 0.2f;
+    private BoxCollider2D AOE;
+
+    private void Start()
+    {
+        AOE = GetComponent<BoxCollider2D>();
+        AOE.size = new Vector2(ColX, ColY);
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
 
