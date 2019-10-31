3565d4d6b7b185349cf2afba016c431d095c1f2c
BenMDS <unigeorge98@gmail.com>
2019-10-31 20:00:36 +1300

    Broken Ass ArcGun

--- Sheep Game/Assets/Scripts/Gun Script/ArcBulletController.cs
+++ Sheep Game/Assets/Scripts/Gun Script/ArcBulletController.cs
@@ -1,42 +1,73 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine; 
+using UnityEngine;
 
-public class ArcBulletController : ArcController
+public class ArcBulletController : MonoBehaviour
 {
     public int damage;
+    public Camera MainCamera;
 
     public float ColX = 0.2f, ColY = 0.2f;
     private BoxCollider2D AOE;
 
-    private Vector2 acceleration;
+    private Rigidbody2D body;
+    private Vector3 mousePosition;
+
+    private float q = 0.25f;
+    private float w = -0.25f;
+
+    private bool gay = false;
+
 
     private void Start()
     {
+        body = GetComponent<Rigidbody2D>();
         AOE = GetComponent<BoxCollider2D>();
         AOE.size = new Vector2(ColX, ColY);
     }
 
     private void Update()
     {
-        Debug.Log(MousePosition);
-
-        if ((GetComponent<Rigidbody2D>().velocity.y < 0.25) && (GetComponent<Rigidbody2D>().velocity.y > -0.25))
+        if ((GetComponent<Rigidbody2D>().velocity.y < q) && (GetComponent<Rigidbody2D>().velocity.y > w))
         {
-            
-
-            float a = GetComponent<Rigidbody2D>().transform.position.y - MousePosition.y;
-            float o = GetComponent<Rigidbody2D>().transform.position.x - MousePosition.x;
+            Debug.Log("Is this wack:" + mousePosition);
+            float a = GetComponent<Rigidbody2D>().transform.position.y - mousePosition.y;
+            float o = GetComponent<Rigidbody2D>().transform.position.x - mousePosition.x;
             float omega = o / a;
             float theta = Mathf.Atan(omega);
             theta = theta * (180.0f / 3.14159f);
 
-            //acceleration.x = a;
-            //acceleration.y = o;
-
             transform.Rotate(0, 0, theta, Space.Self);
-            GetComponent<Rigidbody2D>().velocity = acceleration * 10;
+
+            q = 10f;
+            w = 0f;
+            gay = true;
+            
         }
+
+        if (gay == true)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                mousePosition = MainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y));
+            }
+
+            transform.position = Vector2.MoveTowards(transform.position, mousePosition, 2f);
+
+            Vector3 mouse = MainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
+            Debug.Log("mouse:" + mouse);
+            Vector3 pos = transform.position;
+            float xdiff = mouse.x - pos.x;
+            float ydiff = mouse.y - pos.y;
+
+            if ((xdiff == 0) && (ydiff == 0))
+            {
+                gay = false;
+                Debug.Log(gay);
+            }
+        }
+        
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -57,4 +88,6 @@ public class ArcBulletController : ArcController
             Destroy(gameObject);
         }
     }
+
+
 }
