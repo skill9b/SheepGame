5ecd4d185586b4121fe6f903b6df7f7f5e9a4b1d
StarPatric <unigeorge98@gmail.com>
2019-11-06 19:59:32 +1200

    WINLOSE Data Handler stuff

--- Sheep Game/Assets/Scripts/SceneController.cs
+++ Sheep Game/Assets/Scripts/SceneController.cs
@@ -3,15 +3,33 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public class SceneController : MonoBehaviour
+public static class SceneController
 {
-    public void LoadScene(int lvl)
+    private static int gamelvl; //At the end of the level you need to pass this the level number using getactive().buildindex
+    private static int replay = 1; //At the start of the level if gamelvl != buildindex, reset replay
+
+    public static void LoadScene(int lvl)
     {
         SceneManager.LoadScene(lvl);
-        DontDestroyOnLoad(this.gameObject);
+
+           DontDestroyOnLoad(this.gameObject);
     }
 
-    public void QuitGame()
+    public static void ReplayLvl()
+    {
+        if (replay > 0)
+        {
+            replay--;
+            SceneManager.LoadScene(gamelvl);
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else if (replay == 0)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    public static void QuitGame()
     {
         Application.Quit();
     }
