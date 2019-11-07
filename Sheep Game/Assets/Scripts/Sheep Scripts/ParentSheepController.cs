4cee3dd3d3ba51db9ee796db3866f191e70ab2b0
Ben <Ben>
2019-11-07 16:45:37 +1300

    Update ParentSheepController.cs

--- Sheep Game/Assets/Scripts/Sheep Scripts/ParentSheepController.cs
+++ Sheep Game/Assets/Scripts/Sheep Scripts/ParentSheepController.cs
@@ -77,7 +77,7 @@ public class ParentSheepController : MonoBehaviour
                     currentState = State.Idle;
                     break;
                 }
-                Debug.Log("Current State" + currentState);
+                
         }
 
         // Death
