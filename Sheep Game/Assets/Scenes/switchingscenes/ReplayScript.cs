e046b8b7d276705d827c27bdfb10d43fe3c77b72
BenMDS <unigeorge98@gmail.com>
2019-11-07 14:01:45 +1300

    Fixed

--- Sheep Game/Assets/Scenes/switchingscenes/ReplayScript.cs
+++ Sheep Game/Assets/Scenes/switchingscenes/ReplayScript.cs
@@ -1,28 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.SceneManagement;
 
-public class ReplayScript : MonoBehaviour
-{
-    public SceneController TheController;
+//public class ReplayScript : MonoBehaviour
+//{
+//    public SceneController TheController;
 
-    private void Start()
-    {
-        if (TheController.gamelvl != SceneManager.GetActiveScene().buildIndex)
-        {
-            Debug.Log(SceneManager.GetActiveScene().buildIndex);
-            TheController.replay = 1;
-        }
-    }
+//    private void Start()
+//    {
+//        if (TheController.gamelvl != SceneManager.GetActiveScene().buildIndex)
+//        {
+//            Debug.Log(SceneManager.GetActiveScene().buildIndex);
+//            TheController.replay = 1;
+//        }
+//    }
 
-    private void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            TheController.gamelvl = SceneManager.GetActiveScene().buildIndex;
-            SceneManager.LoadScene(3);
-        }
-    }
+//    private void Update()
+//    {
+//        if (Input.GetMouseButtonDown(0))
+//        {
+//            TheController.gamelvl = SceneManager.GetActiveScene().buildIndex;
+//            SceneManager.LoadScene(3);
+//        }
+//    }
 
-}
+//}
--- Sheep Game/Assets/Scripts/SceneController.cs
+++ Sheep Game/Assets/Scripts/SceneController.cs
@@ -3,34 +3,34 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public static class SceneController
-{
-    private static int gamelvl; //At the end of the level you need to pass this the level number using getactive().buildindex
-    private static int replay = 1; //At the start of the level if gamelvl != buildindex, reset replay
+//public static class SceneController
+//{
+//    private static int gamelvl; //At the end of the level you need to pass this the level number using getactive().buildindex
+//    private static int replay = 1; //At the start of the level if gamelvl != buildindex, reset replay
 
-    public static void LoadScene(int lvl)
-    {
-        SceneManager.LoadScene(lvl);
+//    public static void LoadScene(int lvl)
+//    {
+//        SceneManager.LoadScene(lvl);
 
-           DontDestroyOnLoad(this.gameObject);
-    }
+//           DontDestroyOnLoad(this.gameObject);
+//    }
 
-    public static void ReplayLvl()
-    {
-        if (replay > 0)
-        {
-            replay--;
-            SceneManager.LoadScene(gamelvl);
-            DontDestroyOnLoad(this.gameObject);
-        }
-        else if (replay == 0)
-        {
-            SceneManager.LoadScene(0);
-        }
-    }
+//    public static void ReplayLvl()
+//    {
+//        if (replay > 0)
+//        {
+//            replay--;
+//            SceneManager.LoadScene(gamelvl);
+//            DontDestroyOnLoad(this.gameObject);
+//        }
+//        else if (replay == 0)
+//        {
+//            SceneManager.LoadScene(0);
+//        }
+//    }
 
-    public static void QuitGame()
-    {
-        Application.Quit();
-    }
-}
+//    public static void QuitGame()
+//    {
+//        Application.Quit();
+//    }
+//}
