740f97b7cb90ea0afbdede0c8dfe65e112cfc598
Vivian <viv.xu@windowslive.com>
2019-11-16 23:22:08 +1300

    Tutorial created

--- Sheep Game/Assets/Scripts/SceneController.cs
+++ Sheep Game/Assets/Scripts/SceneController.cs
@@ -20,9 +20,9 @@ public class SceneController : MonoBehaviour
             SceneManager.LoadScene(2);
         }
 
-        if (isShowingTutorial == true)
+        if (SceneManager.GetActiveScene().buildIndex == 7)  // If currently on tutorial scene
         {
-            if (Input.GetKeyDown(KeyCode.KeypadEnter))
+            if (Input.GetKeyDown(KeyCode.Return))
             {
                 StartGame();
             }
@@ -32,11 +32,7 @@ public class SceneController : MonoBehaviour
 
     public void ShowTutorial()
     {
-        if (GameObject.FindGameObjectWithTag("Tutorial") != null)
-        {
-            GameObject.FindGameObjectWithTag("Tutorial").SetActive(true);
-            isShowingTutorial = true;
-        }
+        SceneManager.LoadScene(7);
     }
 
     public void StartGame()
