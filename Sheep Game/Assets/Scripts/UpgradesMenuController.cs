11c961f375076edf28d848bf552db5a6e89402c3
Vivian <viv.xu@windowslive.com>
2019-11-17 21:33:31 +1300

    More UpgradeMenuController changes

--- Sheep Game/Assets/Scripts/UpgradesMenuController.cs
+++ Sheep Game/Assets/Scripts/UpgradesMenuController.cs
@@ -10,7 +10,7 @@ public class UpgradesMenuController : MonoBehaviour
 
     // Player variables
     int playerCurrentWool;
-    float currentWoolCost;
+    int currentWoolCost;
     Sprite currentDescription;
 
     // UI elements to update
@@ -74,7 +74,7 @@ public class UpgradesMenuController : MonoBehaviour
         playerCurrentWool = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>().woolCount;
         playerWoolDisplay.text = playerCurrentWool.ToString();
 
-        currentWoolCost = GetK9CurrentDamage();
+        // currentWoolCost = GetK9CurrentDamage();
         currentWoolCostDisplay.text = currentWoolCost.ToString();
 
         currentDescription = K9DamageDescription;
@@ -84,6 +84,11 @@ public class UpgradesMenuController : MonoBehaviour
     void Update()
     {
         // Display current description & woolcost
+        descriptionBox.sprite = currentDescription;
+        if (currentWoolCost == -1)
+        {
+            currentWoolCostDisplay.text = "";
+        }
 
         // Set current wool points
         playerCurrentWool = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>().woolCount;
@@ -108,7 +113,8 @@ public class UpgradesMenuController : MonoBehaviour
         SupportPanel.SetActive(true);
     }
 
-    void DisplayAllCurrentProgressBars()   // Sets all current progress bars
+    /////////////////////// Sets and displays all current progress bars (ON UPDATE)
+    void DisplayAllCurrentProgressBars()  
     {
         // Humpty Dumpty Fire Rate
         if (GameObject.FindGameObjectWithTag("HumptyDumpty").GetComponent<NPCHumptyDumptyController>().eggCountdown == 1.0f)
@@ -193,22 +199,22 @@ public class UpgradesMenuController : MonoBehaviour
 
         // K9 [ARCGUN]
 
-        // K9 Damage Increase 
-        //if (GameObject.FindGameObjectWithTag("K9Bullet").GetComponent<ArcBulletController>().damage == 10)
+        // K9 Damage Increase
+        //if (GameObject.FindGameObjectWithTag("ArcGun").GetComponent<ArcBulletController>().Damage == 10)
         //{
         //    K9DamageBar.sprite = twoIncrementEmpty;
         //}
-        //else if (GameObject.FindGameObjectWithTag("K9Bullet").GetComponent<ArcBulletController>().maxHealth == 15)
+        //else if (GameObject.FindGameObjectWithTag("ArcGun").GetComponent<ArcBulletController>().Damage == 15)
         //{
         //    K9DamageBar.sprite = twoIncrementHalf;
         //}
-        //else if (GameObject.FindGameObjectWithTag("K9Bullet").GetComponent<ArcBulletController>().maxHealth == 20)
+        //else if (GameObject.FindGameObjectWithTag("ArcGun").GetComponent<ArcBulletController>().Damage == 20)
         //{
         //    K9DamageBar.sprite = twoIncrementFull;
         //}
 
         // K9 Cooldown Increase 
-        //if (GameObject.FindGameObjectWithTag("ArcGun").GetComponent<ArcBulletController>().cooldowntime ?? == 10)
+        //if (GameObject.FindGameObjectWithTag("ArcGun").GetComponent<ArcBulletController>().CooldowntimeNotFull == 10)
         //{
         //    K9CooldownBar.sprite = twoIncrementEmpty;
         //}
@@ -222,34 +228,36 @@ public class UpgradesMenuController : MonoBehaviour
         //}
 
         // Sheariken [LONGGUN]
+        // Sheariken Damage
+        if (GameObject.FindGameObjectWithTag("LongGun").GetComponent<LongController>().Damage == 1.0f)
+        {
+            ShearikenDamageBar.sprite = twoIncrementEmpty;
+        }
+        else if (GameObject.FindGameObjectWithTag("LongGun").GetComponent<LongController>().Damage == 1.5f)
+        {
+            ShearikenDamageBar.sprite = twoIncrementHalf;
+        }
+        else if (GameObject.FindGameObjectWithTag("LongGun").GetComponent<LongController>().Damage == 2.0f)
+        {
+            ShearikenDamageBar.sprite = twoIncrementFull;
+        }
 
-        //if (GameObject.FindGameObjectWithTag("LongBullet").GetComponent<LongBulletController>().damage == 1)
-        //{
-        //    ShearikenDamageBar.sprite = twoIncrementEmpty;
-        //}
-        //else if (GameObject.FindGameObjectWithTag("LongBullet").GetComponent<LongBulletController>().damage == 15)
-        //{
-        //    ShearikenDamageBar.sprite = twoIncrementHalf;
-        //}
-        //else if (GameObject.FindGameObjectWithTag("LongBullet").GetComponent<LongBulletController>().damage == 20)
-        //{
-        //    ShearikenDamageBar.sprite = twoIncrementFull;
-        //}
-
-        if (GameObject.FindGameObjectWithTag("LongBullet").GetComponent<LongBulletController>().PassEnemies == 1)
+        // Sheariken Accuracy
+        if (GameObject.FindGameObjectWithTag("LongBullet").GetComponent<LongController>().PassEnemies == 1)
         {
             ShearikenAccuracyBar.sprite = twoIncrementEmpty;
         }
-        else if (GameObject.FindGameObjectWithTag("LongBullet").GetComponent<LongBulletController>().PassEnemies == 2)
+        else if (GameObject.FindGameObjectWithTag("LongBullet").GetComponent<LongController>().PassEnemies == 2)
         {
             ShearikenAccuracyBar.sprite = twoIncrementHalf;
         }
-        else if (GameObject.FindGameObjectWithTag("LongBullet").GetComponent<LongBulletController>().PassEnemies == 3)
+        else if (GameObject.FindGameObjectWithTag("LongBullet").GetComponent<LongController>().PassEnemies == 3)
         {
             ShearikenAccuracyBar.sprite = twoIncrementFull;
         }
 
         // Old Macdonald [SHOTGUN]
+        // Old MacDonald Cooldown
         if (GameObject.FindGameObjectWithTag("Shotgun").GetComponent<ShortController>().CooldowntimeNotFull == 3.0f)
         {
             OldMacdonaldCooldownBar.sprite = twoIncrementEmpty;
@@ -263,22 +271,23 @@ public class UpgradesMenuController : MonoBehaviour
             OldMacdonaldCooldownBar.sprite = twoIncrementFull;
         }
 
-        //if (GameObject.FindGameObjectWithTag("ShortBullet").transform.localScale.y == 2.5f)
-        //{
-        //    OldMacdonaldAoeBar.sprite = twoIncrementEmpty;
-        //}
-        //else if (GameObject.FindGameObjectWithTag("ShortBullet").transform.localScale.y == 3.5f)
-        //{
-        //    OldMacdonaldAoeBar.sprite = twoIncrementHalf;
-        //}
-        //else if (GameObject.FindGameObjectWithTag("ShortBullet").transform.localScale.y == 4.5f)
-        //{
-        //    OldMacdonaldAoeBar.sprite = twoIncrementFull;
-        //}
+        // Old MacDonald Angle
+        if (GameObject.FindGameObjectWithTag("Shotgun").GetComponent<ShortController>().YScale == 2.5f)
+        {
+            OldMacdonaldAoeBar.sprite = twoIncrementEmpty;
+        }
+        else if (GameObject.FindGameObjectWithTag("Shotgun").GetComponent<ShortController>().YScale == 3.5f)
+        {
+            OldMacdonaldAoeBar.sprite = twoIncrementHalf;
+        }
+        else if (GameObject.FindGameObjectWithTag("Shotgun").GetComponent<ShortController>().YScale == 4.5f)
+        {
+            OldMacdonaldAoeBar.sprite = twoIncrementFull;
+        }
 
     }
 
-    /////////////////////// Get Description sprite functions 
+    /////////////////////// Get upgrade values 
     
     float GetK9CurrentDamage()
     {
@@ -295,120 +304,397 @@ public class UpgradesMenuController : MonoBehaviour
         return GameObject.FindGameObjectWithTag("LongGun").GetComponent<LongController>().Damage;
     }
 
-    float GetShearikenAccuracy()
+    int GetShearikenAccuracy()
     {
         return GameObject.FindGameObjectWithTag("LongGun").GetComponent<LongController>().Damage;
     }
 
+    float GetOldMacdonaldAngle()
+    {
+        return GameObject.FindGameObjectWithTag("ShotGun").GetComponent<ShortController>().YScale;
+    }
 
-    //    // Progress Bar UI Elements - Weapons
-    //public Image ShearikenDamageBar;
-    //public Image ShearikenAccuracyBar;
-    //public Image OldMacdonaldAoeBar;
-    //public Image OldMacdonaldCooldownBar;
+    float GetOldMacdonaldCooldown()
+    {
+        return GameObject.FindGameObjectWithTag("ShotGun").GetComponent<ShortController>().CooldowntimeNotFull;
+    }
 
-    //// Progress Bar UI Elements - Support
-    //public Image BaseHealthBar;
-    //public Image BaseRepairBar;
-    //public Image WolfBlowingPowerBar;
-    //public Image WolfCooldownBar;
-    //public Image HumptyFireRateBar;
-    //public Image HumptySuicideBar;
+    bool GetBaseRegen()
+    {
+        return GameObject.FindGameObjectWithTag("Base").GetComponent<BaseController>().healthRegenActive;
+    }
 
-    /////////////////////// Set Description sprite functions 
+    float GetBaseMaxHealth()
+    {
+        return GameObject.FindGameObjectWithTag("Base").GetComponent<BaseController>().maxHealth;
+    }
+
+    float GetWolfBlowingPower()
+    {
+        return GameObject.FindGameObjectWithTag("Wolf").GetComponent<BigBadWolfController>().blowingCountdown;
+    }
+
+    float GetWolfCooldown()
+    {
+        return GameObject.FindGameObjectWithTag("Wolf").GetComponent<BigBadWolfController>().blowingCooldown;
+    }
+
+    float GetHumptyFireRate()
+    {
+        return GameObject.FindGameObjectWithTag("HumptyDumpty").GetComponent<NPCHumptyDumptyController>().eggCountdown;
+    }
+
+    bool GetHumptySuicide()
+    {
+        return GameObject.FindGameObjectWithTag("HumptyDumpty").GetComponent<NPCHumptyDumptyController>().enableSuicide;
+    }
+
+    /////////////////////// Select/Set Description & textsprite functions (ON CLICK)
 
     void SetK9DamageDescription()
     {
+        //float damage = GetK9CurrentDamage();
+
+        //switch(damage)
+        //{
+        //    case 1.0f:
+        //        currentWoolCost = 100;
+        //        break;
+        //    case 2.0f:
+        //        currentWoolCost = 200;
+        //        break;
+        //    case 3.0f:
+        //        currentWoolCost = -1;
+        //        break;
+        //}
+
         descriptionBox.sprite = K9DamageDescription;
     }
 
     void SetK9CooldownDescription()
     {
+        //float cooldown = GetK9CurrentCooldown();
+
+        //switch (cooldown)
+        //{
+        //    case 1.0f:
+        //        currentWoolCost = 100;
+        //        break;
+        //    case 2.0f:
+        //        currentWoolCost = 200;
+        //        break;
+        //    case 3.0f:
+        //        currentWoolCost = -1;
+        //        break;
+        //}
+
         descriptionBox.sprite = K9CooldownDescription;
     }
 
     void SetShearikenDamageDescription()
     {
+        float damage = GetShearikenDamage();
+
+        switch (damage)
+        {
+            case 1.0f:
+                currentWoolCost = 100;
+                break;
+            case 1.5f:
+                currentWoolCost = 200;
+                break;
+            case 2.0f:
+                currentWoolCost = -1;
+                break;
+        }
+
         descriptionBox.sprite = ShearikenDamageDescription;
     }
 
     void SetShearikenAcurracyDescription()
     {
+        int accuracy = GetShearikenAccuracy();
+
+        switch (accuracy)
+        {
+            case 1:
+                currentWoolCost = 150;
+                break;
+            case 2:
+                currentWoolCost = 200;
+                break;
+            case 3:
+                currentWoolCost = -1;
+                break;
+        }
+
         descriptionBox.sprite = ShearikenAccuracyDescription;
     }
 
     void SetOldMacdonaldAoeDescription()
     {
+        float angle = GetOldMacdonaldAngle();
+
+        switch (angle)
+        {
+            case 2.5f:
+                currentWoolCost = 150;
+                break;
+            case 3.5f:
+                currentWoolCost = 200;
+                break;
+            case 4.5f:
+                currentWoolCost = -1;
+                break;
+        }
+
         descriptionBox.sprite = OldMacdonaldAoeDescription;
     }
 
     void SetOldMacdonaldCooldownDescription()
     {
+        float cooldown = GetOldMacdonaldCooldown();
+
+        switch (cooldown)
+        {
+            case 3.0f:
+                currentWoolCost = 150;
+                break;
+            case 2.5f:
+                currentWoolCost = 200;
+                break;
+            case 2.0f:
+                currentWoolCost = -1;
+                break;
+        }
+
         descriptionBox.sprite = OldMacdonaldCooldownDescription;
     }
 
     void SetBaseHealthDescription()
     {
+        //float cooldown = GetBaseHealth();
+
+        //switch (cooldown)
+        //{
+        //    case 3.0f:
+        //        currentWoolCost = 150;
+        //        break;
+        //    case 2.5f:
+        //        currentWoolCost = 200;
+        //        break;
+        //    case 2.0f:
+        //        currentWoolCost = -1;
+        //        break;
+        //}
+
         descriptionBox.sprite = BaseHealthDescription;
     }
+
+    void SetBaseRegenDescription()
+    {
+        bool isRegenActive = GetBaseRegen();
+
+        if (!isRegenActive)
+        {
+            currentWoolCost = 300;
+        }
+        else
+        {
+            currentWoolCost = -1;
+        }
+    }
+
     void SetWolfBlowingPowerDescription()
     {
+        float blowingPower = GetWolfBlowingPower();
+
+        switch(blowingPower)
+        {
+            case 0.3f:
+                currentWoolCost = 100;
+                break;
+            case 0.4f:
+                currentWoolCost = 200;
+                break;
+            case 0.6f:
+                currentWoolCost = -1;
+                break;
+        }
+
         descriptionBox.sprite = WolfBlowingPowerDescription;
     }
     void SetWolfCooldownDescription()
     {
+        float cooldown = GetWolfCooldown();
+
+        switch (cooldown)
+        {
+            case 11.0f:
+                currentWoolCost = 100;
+                break;
+            case 2.5f:
+                currentWoolCost = 200;
+                break;
+            case 5.5f:
+                currentWoolCost = -1;
+                break;
+        }
+
         descriptionBox.sprite = WolfCooldownDescription;
     }
 
     void SetHumptyFireRateDescription()
     {
+        float fireRate = GetHumptyFireRate();
+
+        switch (fireRate)
+        {
+            case 2.0f:
+                currentWoolCost = 100;
+                break;
+            case 1.5f:
+                currentWoolCost = 200;
+                break;
+            case 1.0f:
+                currentWoolCost = -1;
+                break;
+        }
+
         descriptionBox.sprite = HumptyFireRateDescription;
     }
+
     void SetHumptySuicideDescription()
     {
+        bool isSuicide = GetHumptySuicide();
+
+        if (!isSuicide)
+        {
+            currentWoolCost = 300;
+        }
+        else
+        {
+            currentWoolCost = -1;
+        }
+
         descriptionBox.sprite = HumptySuicideDescription;
     }
 
-    /////////////////////// Set new stat functions
-    public void SetHumptyDumptyFireRate()   // Purchase 
+
+    /////////////////////// Set new stat functions 
+  
+
+    public void PurchaseHumptyDumptyFireRate()   // Purchase 
     {
-        NPCHumptyDumptyController humptyDumpty = GameObject.FindGameObjectWithTag("HumptyDumpty").GetComponent<NPCHumptyDumptyController>();
-        int currentWoolCost = 0;
-
-        if (humptyDumpty.eggCountdown == 2.0)
+        if ((currentWoolCost <= playerCurrentWool) && (currentWoolCost != -1))
         {
-            currentWoolCost = 100;
-        }
-        else if (humptyDumpty.eggCountdown == 1.5)
-        {
-            currentWoolCost = 200;
-        }
-
-        if ( currentWoolCost <= playerCurrentWool)
-        {
+            NPCHumptyDumptyController humptyDumpty = GameObject.FindGameObjectWithTag("HumptyDumpty").GetComponent<NPCHumptyDumptyController>();
             humptyDumpty.eggCountdown -= 0.5f;
             playerCurrentWool -= currentWoolCost;
         }
     }
 
+    public void SetHumptyDumptySuicide()
+    {
+        if ((currentWoolCost <= playerCurrentWool) && (currentWoolCost != -1))
+        {
+            NPCHumptyDumptyController humptyDumpty = GameObject.FindGameObjectWithTag("HumptyDumpty").GetComponent<NPCHumptyDumptyController>();
+            humptyDumpty.enableSuicide = !humptyDumpty.enableSuicide; 
+            playerCurrentWool -= currentWoolCost;
+        }
+    }
+
+    public void SetWolfBlowingPower()
+    {
+        if ((currentWoolCost <= playerCurrentWool) && (currentWoolCost != -1))
+        {
+            BigBadWolfController wolf = GameObject.FindGameObjectWithTag("Wolf").GetComponent<BigBadWolfController>();
+            
+            switch (wolf.blowingCountdown)
+            {
+                case 0.3f:
+                    wolf.blowingCountdown = 0.4f;
+                    break;
+                case 0.4f:
+                    wolf.blowingCountdown = 0.6f;
+                    break;
+                case 0.6f:
+                    break;
+            }
+
+            playerCurrentWool -= currentWoolCost;
+        }
+    }
+
+    public void PurchaseWolfCooldown()
+    {
+        if ((currentWoolCost <= playerCurrentWool) && (currentWoolCost != -1))
+        {
+            BigBadWolfController wolf = GameObject.FindGameObjectWithTag("Wolf").GetComponent<BigBadWolfController>();
+
+            switch (wolf.blowingCountdown)
+            {
+                case 11.0f:
+                    wolf.blowingCountdown = 7.5f;
+                    break;
+                case 7.5f:
+                    wolf.blowingCountdown = 5.5f;
+                    break;
+                case 0.6f:
+                    break;
+            }
+
+            playerCurrentWool -= currentWoolCost;
+        }
+    }
+
+    public void PurchaseBaseRegen()
+    {
+        if ((currentWoolCost <= playerCurrentWool) && (currentWoolCost != -1))
+        {
+            BaseController baseObject = GameObject.FindGameObjectWithTag("Base").GetComponent<BaseController>();
+
+            baseObject.healthRegenActive = !baseObject.healthRegenActive;
+
+            playerCurrentWool -= currentWoolCost;
+        }
+    }
+
+    public void PurchaseHealthIncrease()
+    {
+        if ((currentWoolCost <= playerCurrentWool) && (currentWoolCost != -1))
+        {
+            BaseController baseObject = GameObject.FindGameObjectWithTag("Base").GetComponent<BaseController>();
+
+            switch (baseObject.maxHealth)
+            {
+                case 10:
+                    baseObject.maxHealth = 10;
+                    break;
+                case 15:
+                    baseObject.maxHealth = 20;
+                    break;
+                case 20:
+                    baseObject.maxHealth = 25;
+                    break;
+                case 25:
+                    break;
+            }
+
+            playerCurrentWool -= currentWoolCost;
+        }
+    }
 
     ////////////// Weapon Costs
-    // int[] K9DamageCosts = new int[] { 100, 200 };
-    // int[] K9CooldownCosts = new int[] { 100, 200 };
+    //int[] K9DamageCosts = new int[] { 100, 200 };
+    //int[] K9CooldownCosts = new int[] { 100, 200 };
     //int[] ShearikenDamageCosts = new int[] { 100, 200 };
     //int[] ShearikenAccuracyCosts = new int[] { 150, 200 };
     //int[] OldMacdonaldAoeCosts = new int[] { 100, 200 };
     //int[] OldMacdonaldCooldownCosts = new int[] { 100, 200 };
 
-    ////////////// Support Costs
-    //int[] HumptyFireRateCosts = new int[] { 100, 200 };
-    //int HumptySuicideCost = 300;
-    //int[] WolfBlowingPowerCost = new int[] { 100, 200 };
-    //int[] WolfCooldownCost = new int[] { 100, 200 };
-    //int BaseRegenCost = 200;
-    //int BaseHealthIncreaseCost = 200;
 
-    public void EnableHumptyDumptySuicide()
+    public void PurchaseHumptyDumptySuicide()
     {
         GameObject.FindGameObjectWithTag("HumptyDumpty").GetComponent<NPCHumptyDumptyController>();
     }
